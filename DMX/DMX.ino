804f41cd7f332c92d593a218c9a506dfb3c3e3d2
Akash Sen <6294426+aksavio@users.noreply.github.com>
2023-01-17 23:35:34 +0530

    Update DMX.ino
    organized the code a bit more, decided not to make header files

--- DMX/DMX.ino
+++ DMX/DMX.ino
@@ -15,18 +15,19 @@ Process:
   1. Make the rotary encoder work
       a. is it RGB or something else
       b. Change value 
-
+  2. Learn how to make your own header files
+  3. Make a Basic inerface on the OLED
 
 
 TODO:
-try to make the program more scalable with structues
-try to use boolean instead of integers
+try to make the program more scalable with structues      :Done
+try to use boolean instead of integers                    :Done
 */
 
 
 // Output pins of the rotary encoder
-#define outputA 6
-#define outputB 7
+#define rotary_F 6
+#define rotary_B 7
 // Variables for the rotary encoder
 
 struct rotary {
@@ -34,40 +35,55 @@ struct rotary {
   bool rState;
   bool rLastState;
 }rotary1; 
-// Mode will be the DMX channel of the light
+
+
+byte rgbw[4];
+
+
+void rotary_init(){
+  pinMode (rotary_F,INPUT);
+  pinMode (rotary_B,INPUT);
+  rotary1.rState = 0;
+  rotary1.rLastState = 0;
+  rotary1.rLastState = digitalRead(rotary_F); 
+}
+
+void read_rotary(){
+  
+     
+      if (digitalRead(rotary_B) != rotary1.rState) { 
+       rotary1.counter ++;
+      } else {
+       rotary1.counter --;
+      }
+     
+   
+   rotary1.rLastState = rotary1.rState; // Updates the previous state of the outputA with the current state
+   
+}
+
 
 void setup() {
   Serial.begin (9600);
   //Version
   Serial.println("DMX ");
   Serial.println("Author:   Akash Savio Sen ");
-  Serial.println("Version:  1.0       Alpha ");
+  Serial.println("Version:  0.1       Alpha ");
   Serial.println("Date:     11.01.23        ");
   Serial.println("Time:     something       ");
-  pinMode (outputA,INPUT);
-  pinMode (outputB,INPUT);
   
-  rotary1.rState = 0;
-  rotary1.rLastState = 0;
-  rotary1.rLastState = digitalRead(outputA);  
+  
+   void rotary_init();
 
 }
 
 void loop() {
-  rotary1.rState = digitalRead(outputA); // Reads the "current" state of the outputA
-   // If the previous and the current state of the outputA are different, that means a Pulse has occured
-   if (rotary1.rState != rotary1.rLastState){     
-     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-     if (digitalRead(outputB) != rotary1.rState) { 
-       rotary1.counter ++;
-     } else {
-       rotary1.counter --;
-     }
-     Serial.print("Position: ");
-     Serial.println(rotary1.counter);
-   } 
-   rotary1.rLastState = rotary1.rState; // Updates the previous state of the outputA with the current state
+  rotary1.rState = digitalRead(rotary_F);
+  if (rotary1.rState != rotary1.rLastState){
+    read_rotary();
+  }
   
-
+  Serial.print("Position: ");
+  Serial.println(rotary1.counter);
 
 }
