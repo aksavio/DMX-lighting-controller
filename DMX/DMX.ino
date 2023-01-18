3b841ba0c7cb8bdc2108ca4118d6bed5a5eec37f
Akash Sen <6294426+aksavio@users.noreply.github.com>
2023-01-19 00:57:11 +0530

    Added oled functionality

--- DMX/DMX.ino
+++ DMX/DMX.ino
@@ -17,6 +17,8 @@ Process:
       b. Change value 
   2. Learn how to make your own header files
   3. Make a Basic inerface on the OLED
+      a. display the serial oitput on the oled
+      
 
 
 TODO:
@@ -24,6 +26,17 @@ try to make the program more scalable with structues      :Done
 try to use boolean instead of integers                    :Done
 */
 
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     8 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Output pins of the rotary encoder
 #define rotary_F 6
@@ -72,8 +85,22 @@ void setup() {
   Serial.println("Date:     11.01.23        ");
   Serial.println("Time:     something       ");
   
+
+  //Display Check
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  Serial.println(F("SSD1306 allocation failed"));
+  for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Mode Value");
+  display.display();
   
-   void rotary_init();
+  display.display();
+  void rotary_init();
 
 }
 
@@ -84,6 +111,20 @@ void loop() {
   }
   
   Serial.print("Position: ");
+  rgbw[0] = rotary1.counter;
   Serial.println(rotary1.counter);
+  display.clearDisplay();
+  
+  display.setCursor(0,0);
+  display.print("Red    ");
+  display.println(rgbw[0]);
+  display.print("Green  ");
+  display.println(rgbw[0]);
+  display.print("Blue   ");
+  display.println(rgbw[0]);
+  display.print("White  ");
+  display.println(rgbw[0]);
+  
+  display.display();
 
 }
